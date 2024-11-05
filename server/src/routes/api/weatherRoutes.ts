f31468400a02165a30b453e49db1955447b7f173
Thida612 <angelic722@gmail.com>
2024-11-04 21:44:53 -0600

    added weather data from city name and save city to search history

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -10,7 +10,17 @@ router.post('/', async (req, res) => {
     const { cityName } = req.body;
     console.log('Received cityName:', cityName);
   // TODO: GET weather data from city name
+  const weatherData = await WeatherService.getWeatherForCity(cityName);
+    console.log('Fetched weatherData:', weatherData);
+
   // TODO: save city to search history
+  await HistoryService.addCity(cityName);
+
+  res.status(200).json(weatherData);
+} catch (error) {
+  console.error('Error retrieving weather data:', error);
+  res.status(500).json({ message: 'Error retrieving weather data' });
+}
 });
 
 // TODO: GET search history
