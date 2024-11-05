07a20bf97f747f7bc6887f4adc41919840b290f1
Thida612 <angelic722@gmail.com>
2024-11-04 21:42:11 -0600

    added request with city name to retrieve weather data

---
+++ server/src/routes/api/weatherRoutes.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+const router = Router();
+
+ import HistoryService from '../../service/historyService.js';
+ import WeatherService from '../../service/weatherService.js';
+
+// TODO: POST Request with city name to retrieve weather data
+router.post('/', async (req, res) => {
+  try {
+    const { cityName } = req.body;
+    console.log('Received cityName:', cityName);
+  // TODO: GET weather data from city name
+  // TODO: save city to search history
+});
+
+// TODO: GET search history
+router.get('/history', async (req, res) => {});
+
+// * BONUS TODO: DELETE city from search history
+router.delete('/history/:id', async (req, res) => {});
+
+export default router;
